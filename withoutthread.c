554b381004a866c9b3fb1cb7954f48e70cc6784b
RealTimeGroup <35651988+RealTimeGroup@users.noreply.github.com>
2018-06-21 15:21:16 +0300

    Create withoutthread.c

---
+++ withoutthread.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+
+int main(){
+	while(1){
+		//wait for data from the sever
+		readfromserver()
+		{
+			//do something
+		}
+		//wait for data from the keyboard
+		readfromkeyboard()
+		{
+			//do something
+		}
+	}
+	return 0;
+}
